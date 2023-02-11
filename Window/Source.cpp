85b2de1feb0cb4df16570e6d78e9c7ab2ced68fb
SAP4K <indexionn@gmail.com>
2023-02-10 17:38:16 -0800

    CurbaPrinFunctie

--- Window/Source.cpp
+++ Window/Source.cpp
@@ -1,20 +1,31 @@
 #include <windows.h>
-#include<wingdi.h>
 #include<iostream>
 const wchar_t ClassName[] = L"myWindowClass";
 HINSTANCE hin;
-int x = 300;
-int y = 400;
-POINT points[4] = { 20, 40, x, y, 330, 110, 450, 50 };
+int x[4] = {20,500,330,450};
+int y[4] = {40,600,110,50};
+//pentru ca sai schimbi forma la curba modifica valorile
+POINT points[4] = { 20, 40, 300, 200, 330, 110, 450, 50};
+//Curata ecranul inainte de a fi desenat
 void ClearSurface(HWND hwnd) {
     RECT rcWnd;
     GetClientRect(hwnd, &rcWnd);
     HDC hdc = GetWindowDC(hwnd);
-    rcWnd.bottom += 50;
-    rcWnd.right += 50;
-    FillRect(hdc, &rcWnd, (HBRUSH)(COLOR_WINDOW + 2));
+    rcWnd.right += 20;
+    rcWnd.bottom += 30;
+    FillRect(hdc, &rcWnd, (HBRUSH)(COLOR_WINDOW + 1));
     ReleaseDC(hwnd, hdc);
-
+}
+//Redeseneaza curba
+void DrawNewPosition(HWND hwnd,int x,int y) {
+    std::cout << "este\n";
+    HDC hdc = GetWindowDC(hwnd);
+    for (int i = 0; i < 4; i++) {
+        points[i].x += x;
+        points[i].y += y;
+    }
+    PolyBezier(hdc, points, 4);
+    ReleaseDC(hwnd, hdc);
 }
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -27,8 +38,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
         PAINTSTRUCT pn;
         RECT rc;
         HDC hdc = BeginPaint(hwnd, &pn);
+        ClearSurface(hwnd);
         PolyBezier(hdc, points, 4);
-        
         EndPaint(hwnd, &pn);
     }break;
     case WM_KEYDOWN: {
@@ -38,7 +49,24 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
             ClearSurface(hwnd);
         }
         case VK_UP: {
-            std::cout << "tes \n";
+            std::cout << "UP\n";
+            ClearSurface(hwnd);
+            DrawNewPosition(hwnd,0,10); 
+        }break;
+        case VK_DOWN:{
+            std::cout << "DOWN\n";
+            ClearSurface(hwnd);
+            DrawNewPosition(hwnd, 0, -10);
+        }break;
+        case VK_LEFT: {
+            std::cout << "Left\n";
+            ClearSurface(hwnd);
+            DrawNewPosition(hwnd, -10, 0);
+        }break;
+        case VK_RIGHT: {
+            std::cout << "Right\n";
+            ClearSurface(hwnd);
+            DrawNewPosition(hwnd, 10, 0);
         }break;
         }
     }break;
